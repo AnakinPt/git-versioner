7ac3bf77357627a342f05f71388fdfb96d88d834
Hugo Dias <hugo.dias@criticaltechworks.com>
2022-12-02 14:46:56 +0000

    Test authentication

--- src/main/java/pt/com/hugodias/gradle/gitversioner/core/tag/GitTagger.java
+++ src/main/java/pt/com/hugodias/gradle/gitversioner/core/tag/GitTagger.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.TagCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -15,6 +16,7 @@ import pt.com.hugodias.gradle.gitversioner.core.exception.TaggingException;
 
 @Data
 @Builder
+@Slf4j
 public class GitTagger {
   private File gitFolder;
   private TaggerConfig config;
@@ -26,9 +28,12 @@ public class GitTagger {
       String prefixedVersion = config.getPrefix() + version;
       TagCommand tagCommand = git.tag().setName(prefixedVersion);
       if (config.isUseCommitMessage()) {
+        log.debug("Using last commit message");
         tagCommand.setMessage(getLastCommitMessage(git));
       }
       tagCommand.call();
+      log.debug("Pushing the new tag with name "+prefixedVersion);
+      log.debug("Credential Provider "+credentialsProvider.toString());
       git.push().add(prefixedVersion).setCredentialsProvider(credentialsProvider).call();
     } catch (IOException | GitAPIException e) {
       throw new TaggingException(e);
@@ -37,15 +42,20 @@ public class GitTagger {
 
   private void configureHostChecking(TaggerConfig config) {
     JSch.setConfig("StrictHostKeyChecking", config.isStrictHostChecking() ? "yes" : "no");
+    log.debug("Adding StrictHostKeyChecking with "+config.isStrictHostChecking());
   }
 
   private CredentialsProvider createCredentialsProvider(TaggerConfig config) {
     if (config.getUsername() != null) {
+      log.debug("Creating a Credential Provider with username and password");
+      log.trace("Username: "+config.getUsername());
       return new UsernamePasswordCredentialsProvider(config.getUsername(), config.getPassword());
     }
     if (config.getToken() != null) {
+      log.debug("Creating a Credential Provider with token");
       return new UsernamePasswordCredentialsProvider(config.getToken(), "");
     }
+    log.debug("Creating the default Credential Provider");
     return CredentialsProvider.getDefault();
   }
 
